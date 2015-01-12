b9e14ba94a3420c47cb9de144c55cc41124d16ce
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-12 09:20:52 +0900

    Change: remove main_spec, add home_screen_spec

--- spec/main_spec.rb
+++
@@ -1,9 +0,0 @@
-describe "Application 'pm_search_and_long_press'" do
-  before do
-    @app = UIApplication.sharedApplication
-  end
-
-  it "has one window" do
-    @app.windows.size.should == 1
-  end
-end
