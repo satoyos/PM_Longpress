bc8a8079952fabf9abbd2f624ea203c6885b7027
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-12 09:22:20 +0900

    Change: HomeScreenをPM::TableScreenのサブクラスに変更した。
     - TableScreenプロトコルに従い、table_dataメソッドを実装した。

--- app/home_screen.rb
+++ app/home_screen.rb
@@ -1,7 +1,23 @@
-class HomeScreen < PM::Screen
+class HomeScreen < PM::TableScreen
   def on_load
     set_attributes self.view, {
       background_color: hex_color('#FFFFFF')
+    }
+  end
+
+  def table_data
+    [{
+         cells: test_cells
+     }]
+  end
+
+  private
+
+  def test_cells
+    (1..20).to_a.map{|num|
+      {
+          title: "aaa#{num}"
+      }
     }
   end
 end
