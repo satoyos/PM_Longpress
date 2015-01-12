85120717228a233ab8e606cc05b96687428cbf8c
Yoshifumi Sato <sato_yos@nifty.com>
2015-01-12 20:54:26 +0900

    Add: "longpressable" feature to HomeScreen.

--- app/home_screen.rb
+++ app/home_screen.rb
@@ -1,5 +1,6 @@
 class HomeScreen < PM::TableScreen
   searchable
+  longpressable
 
   def on_load
     set_attributes self.view, {
@@ -17,6 +18,10 @@ class HomeScreen < PM::TableScreen
     puts "#{args[:name]} tapped!"
   end
 
+  def cell_long_pressed(args)
+    puts "#{args[:name]} Long Pressed!!!"
+  end
+
   private
 
   def test_cells
@@ -28,6 +33,7 @@ class HomeScreen < PM::TableScreen
     {
         title: cell_title,
         action: :cell_tapped,
+        long_press_action: :cell_long_pressed,
         arguments: {name: cell_title}
     }
   end
